af99d6ec5bbc0993a1151429ba8a732e915d2151
RG-67 <rittikghoshkolkata@gmail.com>
2024-10-16 09:55:36 +0530

    api params loaded

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const {products, people} = require('./data')
 
-app.get('/app', function (req, res) {
-    res.status(200).end(`Node world..`)
+app.get('/app/products', function (req, res) {
+    const filteredProduct = products.map((products) => {
+        const {name, description} = products
+        return {name, description}
+    })
+    res.json(filteredProduct)
+})
+app.get('/app/singleProduct/:productId', (req, res) => {
+    const {productId} = req.params
+    const singleProduct = products.find((product) => product.id === Number(productId))
+    if (!singleProduct) {
+        return res.json({'status': false, 'msg': 'product not found'})
+    }
+    return res.send(singleProduct)
 })
 app.all('*', (req, res) => {
     res.status(404).send('Not found..')
---
+++ data.js
@@ -0,0 +1,101 @@
+const products = [
+    {
+        id: 1,
+        name: 'product1',
+        description: 'It is 1st product'
+    },
+    {
+        id: 2,
+        name: 'product2',
+        description: 'It is 2nd product'
+    },
+    {
+        id: 3,
+        name: 'product3',
+        description: 'It is 3rd product'
+    },
+    {
+        id: 4,
+        name: 'product4',
+        description: 'It is 4th product'
+    },
+    {
+        id: 5,
+        name: 'product5',
+        description: 'It is 5th product'
+    },
+    {
+        id: 6,
+        name: 'product6',
+        description: 'It is 6th product'
+    },
+    {
+        id: 7,
+        name: 'product7',
+        description: 'It is 7th product'
+    },
+    {
+        id: 8,
+        name: 'product8',
+        description: 'It is 8th product'
+    },
+    {
+        id: 9,
+        name: 'product9',
+        description: 'It is 9th product'
+    },
+    {
+        id: 10,
+        name: 'product10',
+        description: 'It is 10th product'
+    }
+]
+
+const people = [
+    {
+        id: 1,
+        name: 'Rittik1'
+    },
+    {
+        id: 1,
+        name: 'Rittik1'
+    },
+    {
+        id: 2,
+        name: 'Rittik2'
+    },
+    {
+        id: 3,
+        name: 'Rittik3'
+    },
+    {
+        id: 4,
+        name: 'Rittik4'
+    },
+    {
+        id: 5,
+        name: 'Rittik5'
+    },
+    {
+        id: 6,
+        name: 'Rittik6'
+    },
+    {
+        id: 7,
+        name: 'Rittik7'
+    },
+    {
+        id: 8,
+        name: 'Rittik8'
+    },
+    {
+        id: 9,
+        name: 'Rittik9'
+    },
+    {
+        id: 10,
+        name: 'Rittik10'
+    },
+]
+
+module.exports = {products, people}
