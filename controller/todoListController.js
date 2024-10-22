712ca95ad75bb1b6d8a0436b330b396bb7def0db
RG-67 <rittikghoshkolkata@gmail.com>
2024-10-22 23:15:00 +0530

    validation added

--- controller/todoListController.js
+++ controller/todoListController.js
@@ -1,19 +1,54 @@
 const note = require('../models/note')
 
+// assert.equal(err.errors['note'].message, 'note should not empty')
+    // assert.equal(err.errors['userId'].message, 'user id should not empty or already exists')
+    // assert.equal(err.errors['date'].message, 'data should not empty')
+    // assert.equal(err.errors['time'].message, 'time should not empty')
 
 const createNote = async (req, res) => {
-    const notes = await note.create(req.body)
-    res.status(200).json({notes})
+    try {
+        const notes = await note.create(req.body)
+        res.status(200).json({notes})   
+    } catch (error) {
+        let noteErrMsg = "", userErrMsg = "", dateErrMsg = "", timeErrMsg = "";
+        if (error.name === 'ValidationError') {
+            if (error.errors.note) {
+                noteErrMsg = error.errors.note.message
+            }   
+            if (error.errors.userId) {
+                userErrMsg = error.errors.userId.message
+            }
+            if (error.errors.date) {
+                dateErrMsg = error.errors.date.message
+            }
+            if (error.errors.time) {
+                timeErrMsg = error.errors.time.message
+            }
+        }
+        if (error.code === 11000 && error.keyValue.userId) {            
+            res.status(200).send({status: false, userError: `${error.keyValue.userId} already exists`})
+        } else {
+            res.status(200).send({status: false, ErrorMsg: [{noteError: noteErrMsg}, {userError: userErrMsg}, {dateError: dateErrMsg}, {timeErrMsg: timeErrMsg}]})   
+        }
+    }
 }
 
 const getAllNotes = async(req, res) => {
     const notes = await note.find();
-    res.status(200).send({status: 'data successfully retrieved', data: notes})
+    if (notes !== null) {
+        res.status(200).send({status: true, msg: 'data successfully retrieved', data: []})   
+    } else {
+        res.status(200).send({status: false, msg: 'data not found', data: notes})
+    }
 }
 
 const getNoteByUserId = async(req, res) => {
     const userNote = await note.findOne({userId: req.params.userId}).exec()
-    res.status(200).send({status: 'data successfully retrieved', data: userNote})
+    if (userNote !== null) {
+        res.status(200).send({status: true, msg: 'data successfully retrieved', data: userNote})
+    } else {
+        res.status(200).send({status: false, msg: 'invalid user id', data: {}})
+    }
 }
 
 module.exports = {
--- models/note.js
+++ models/note.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose')
 
 const createNote = new mongoose.Schema({
-    note: String, 
-    userId: String, 
-    date: String, 
-    time: String
+    note: {
+        type: String,
+        required: [true, 'note should not empty']
+    },
+    userId: {
+        type: String,
+        required: [true, 'user id should not empty'],
+        unique: true
+    },
+    date: {
+        type: String,
+        required: [true, 'date should not empty']
+    },
+    time: {
+        type: String,
+        required: [true, 'time should not empty']
+    }
 })
 
 
-module.exports = mongoose.model('Notes', createNote)
+module.exports = mongoose.model('notes', createNote)
+
