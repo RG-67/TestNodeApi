5d9dd97823bc517cf4d47a8a56a80264135a086f
RG-67 <rittikghoshkolkata@gmail.com>
2024-12-16 21:13:10 +0530

    Temporary: Add ignored files for deployment

---
+++ utility/appCred.js
@@ -0,0 +1,3 @@
+const token = "d98YmqatRpeGp-mon835kE:APA91bHBm-3G8c6xEreAxLYtnkAn8uycqXP2I1f2rOx4VL9RIadwoYQtnHzLVp6_t3psE-8QsTjRR7S6r37RfDeQzErPwEit4vguEtV4YgYjvaV08vcrQS0"
+
+module.exports = { token };
